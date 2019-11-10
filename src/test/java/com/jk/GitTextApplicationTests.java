f1a0d7a2fa11f1fe61f5216c735746d53c7ff879
mniszsq <1974919893@qq.com>
2019-11-10 16:49:47 +0800

    第一次提交

---
+++ src/test/java/com/jk/GitTextApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTextApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("第一次提交");
+    }
+
+}
