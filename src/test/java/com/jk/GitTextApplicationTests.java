df79866a4d5b062b95f24d1d187a1d8c7238bfd8
mniszsq <1974919893@qq.com>
2019-11-10 16:56:03 +0800

    分支提交

--- src/test/java/com/jk/GitTextApplicationTests.java
+++ src/test/java/com/jk/GitTextApplicationTests.java
@@ -11,6 +11,7 @@ class GitTextApplicationTests {
         System.out.println("第一次提交");
         System.out.println("第二次提交");
         System.out.println("第三次提交");
+        System.out.println("分支");
     }
 
 }
